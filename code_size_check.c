38ac74f87016972e5f7e3df2cc1b456d9f8fcba3
Laurence Lundblade <llundbla@qti.qualcomm.com>
2018-09-18 19:50:50 -0700

    don't check in code_size_checks

--- code_size_check.c
+++
@@ -1,9 +0,0 @@
-//
-//  code_size_check.c
-//  QCBOR
-//
-//  Created by Laurence Lundblade on 9/15/18.
-//  Copyright © 2018 Laurence Lundblade. All rights reserved.
-//
-
-#include "code_size_check.h"
