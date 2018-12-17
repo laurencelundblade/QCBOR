d4728fd6f1054219035f05df907c9a41b1e2b80d
Laurence Lundblade <lgl@island-resort.com>
2018-12-17 15:15:56 -0800

    strings.h -> string.h (was just a typo)

--- test/qcbor_decode_tests.c
+++ test/qcbor_decode_tests.c
@@ -32,7 +32,7 @@ IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
 
 #include "qcbor.h"
 #include "qcbor_decode_tests.h"
-#include <strings.h>
+#include <string.h>
 #include <math.h> // for fabs()
 #include <stdlib.h>
 
