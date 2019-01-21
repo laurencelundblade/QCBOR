a3fd49fc80c090e4ac4ba3f5c9fe200dbd1db50f
Laurence Lundblade <lgl@securitytheory.com>
2019-01-21 10:16:22 -0800

    Use SPDX-License-Identifier. (No change to code or license terms)

--- test/half_to_double_from_rfc7049.h
+++ test/half_to_double_from_rfc7049.h
@@ -1,44 +1,14 @@
 /*==============================================================================
-
- Copyright (c) 2018, Laurence Lundblade.
- All rights reserved.
-
- Copyright (c) 2018, Laurence Lundblade.
- All rights reserved.
-
-Redistribution and use in source and binary forms, with or without
-modification, are permitted provided that the following conditions are
-met:
-    * Redistributions of source code must retain the above copyright
-      notice, this list of conditions and the following disclaimer.
-    * Redistributions in binary form must reproduce the above
-      copyright notice, this list of conditions and the following
-      disclaimer in the documentation and/or other materials provided
-      with the distribution.
-    * The name "Laurence Lundblade" may not be used to
-      endorse or promote products derived from this software without
-      specific prior written permission.
-
-THIS SOFTWARE IS PROVIDED "AS IS" AND ANY EXPRESS OR IMPLIED
-WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES OF
-MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NON-INFRINGEMENT
-ARE DISCLAIMED.  IN NO EVENT SHALL THE COPYRIGHT OWNER OR CONTRIBUTORS
-BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
-CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
-SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR
-BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY,
-WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE
-OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN
-IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
- ==============================================================================*/
-
-//
-//  half_to_double_from_rfc7049.h
-//  QCBOR
-//
-//  Created by Laurence Lundblade on 9/23/18.
-//  Copyright © 2018 Laurence Lundblade. All rights reserved.
-//
+ half_to_double_from_rfc7049.h -- interface to IETF float conversion code.
+ 
+ Copyright (c) 2018-2019, Laurence Lundblade. All rights reserved.
+ 
+ SPDX-License-Identifier: BSD-3-Clause
+ 
+ See BSD-3-Clause license in README.md
+ 
+ Created on 9/23/18
+  ==============================================================================*/
 
 #ifndef half_to_double_from_rfc7049_h
 #define half_to_double_from_rfc7049_h
--- test/run_tests.c
+++ test/run_tests.c
@@ -1,37 +1,14 @@
 /*==============================================================================
  run_tests.c -- test aggregator and results reporting
+ 
+ Copyright (c) 2018-2019, Laurence Lundblade. All rights reserved.
+ 
+ SPDX-License-Identifier: BSD-3-Clause
+ 
+ See BSD-3-Clause license in README.md
 
  Created on 9/30/18
-
- Copyright (c) 2018-2019, Laurence Lundblade.
- All rights reserved.
-
-Redistribution and use in source and binary forms, with or without
-modification, are permitted provided that the following conditions are
-met:
-    * Redistributions of source code must retain the above copyright
-      notice, this list of conditions and the following disclaimer.
-    * Redistributions in binary form must reproduce the above
-      copyright notice, this list of conditions and the following
-      disclaimer in the documentation and/or other materials provided
-      with the distribution.
-    * The name "Laurence Lundblade" may not be used to
-      endorse or promote products derived from this software without
-      specific prior written permission.
-
-THIS SOFTWARE IS PROVIDED "AS IS" AND ANY EXPRESS OR IMPLIED
-WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES OF
-MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NON-INFRINGEMENT
-ARE DISCLAIMED.  IN NO EVENT SHALL THE COPYRIGHT OWNER OR CONTRIBUTORS
-BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
-CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
-SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR
-BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY,
-WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE
-OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN
-IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
  ==============================================================================*/
-
 
 #include "run_tests.h"
 #include "UsefulBuf.h"
