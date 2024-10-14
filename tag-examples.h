e91efe49c08f8fc2a5fc8fc71fdd7eb0336557e9
Laurence Lundblade <lgl@securitytheory.com>
2024-10-13 19:00:06 -0700

    Add missing tag-example files

---
+++ tag-examples.h
@@ -0,0 +1,16 @@
+//
+//  tag-examples.h
+//  QCBOR
+//
+//  Created by Laurence Lundblade on 10/7/24.
+//  Copyright © 2024 Laurence Lundblade. All rights reserved.
+//
+
+#ifndef tag_examples_h
+#define tag_examples_h
+
+#include <stdint.h>
+
+int32_t RunTagExamples(void);
+
+#endif /* tag_examples_h */
