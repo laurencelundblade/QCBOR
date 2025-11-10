1f63ae5412e48a2c021dcbd1bc13cd5d07dfc327
Laurence Lundblade <lgl@securitytheory.com>
2025-11-10 13:46:37 -0500

    hex literals and copyrights

--- test/qcbor_encode_tests.c
+++ test/qcbor_encode_tests.c
@@ -1,6 +1,6 @@
 /*==============================================================================
  Copyright (c) 2016-2018, The Linux Foundation.
- Copyright (c) 2018-2024, Laurence Lundblade.
+ Copyright (c) 2018-2025, Laurence Lundblade.
  Copyright (c) 2022, Arm Limited. All rights reserved.
 
 Redistribution and use in source and binary forms, with or without
