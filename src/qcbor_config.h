bc6ff35b1d0fe41f75a4e8673428209e53ee3399
Laurence Lundblade <lgl@securitytheory.com>
2026-04-30 13:20:04 -0700

    New ifdef fan out build and debug strategy

---
+++ src/qcbor_config.h
@@ -0,0 +1,13 @@
+//
+//  Header.h
+//  QCBOR
+//
+//  Created by Laurence Lundblade on 4/30/26.
+//  Copyright © 2026 Laurence Lundblade. All rights reserved.
+//
+
+#ifndef Header_h
+#define Header_h
+
+
+#endif /* Header_h */
